e351e6eed02a06d2e58f3560ed52d6db5e7ce905
AlexanderA29 <alexanderarjona93@gmail.com>
2025-05-13 23:36:13 -0500

    Resumen de los cambios del día: - blog_detail.html: Se implementó la restricción para que un usuario solo pueda agregar una review por blog. Se añadieron comentarios explicativos. - review_form.html: Se añadió feedback visual al hacer hover, se refactorizó el JavaScript y se implementaron variables CSS. - views.py: Se modificó paginate_by para mostrar 5 blogs por página. - blog_list.html: Se ajustaron estilos y script para la transición de página, se corrigieron problemas de paginación. - base.html: Se corrigió el flash blanco y se añadió precarga de estilos de Tailwind.

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,13 +1,34 @@
 from django.views.generic import ListView, DetailView, CreateView
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from .models import Blog, Review, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import get_object_or_404
-from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 class BlogListView(ListView):
     model = Blog
     template_name = 'blogapp/blog_list.html'
+    context_object_name = 'blogs'
+    paginate_by = 5  # Mostrar 5 blogs por página
+
+    def get_queryset(self):
+        return Blog.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        blog_list = self.get_queryset()
+        paginator = Paginator(blog_list, self.paginate_by)
+
+        page = self.request.GET.get('page')
+
+        try:
+            blogs = paginator.page(page)
+        except PageNotAnInteger:
+            blogs = paginator.page(1)
+        except EmptyPage:
+            blogs = paginator.page(paginator.num_pages)
+
+        context['blogs'] = blogs
+        return context
 
 
 class BlogDetailView(DetailView):
@@ -35,19 +56,12 @@ class ReviewCreateView(CreateView):
     template_name = 'blogapp/review_form.html'
 
     def form_valid(self, form):
-        blog = get_object_or_404(Blog, pk=self.kwargs['pk'])
-        reviewer = self.request.user
-        # Verificar si el usuario ya ha creado una review para este blog
-        existing_review = Review.objects.filter(blog=blog, reviewer=reviewer).exists()
-        if existing_review:
-            messages.error(self.request, "You have already reviewed this blog.")
-            return self.form_invalid(form)
-        form.instance.blog = blog
-        form.instance.reviewer = reviewer
+        form.instance.reviewer = self.request.user
+        form.instance.blog_id = self.kwargs['pk']
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('blogapp:blog_detail', kwargs={'pk': self.kwargs['pk']})
+        return reverse_lazy('blogapp:blog_detail', kwargs={'pk': self.kwargs['pk']})
 
 
 class CommentCreateView(CreateView):
